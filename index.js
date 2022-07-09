ce5e190806c9a2ebac22202cffce6a29262d0526
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-09 20:43:55 -0300

    Add variáveis e escolha do número de rodadas

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const prompt = require('prompt-sync')();
 console.clear();
 
+let pointsUs = 0;
+let pointsPc = 0;
+let numberPlays = 1;
+const options = ['', 'Pedra', 'Papel', 'Tesoura'];
+
+console.log('|============= JoKenPô ============|');
+console.log('|  Quantas rodadas iremos jogar?   |');
+let rounds = +prompt('                 ');
+console.log('|==================================|');
+
+for (let i = 1; i <= rounds; i++) {}
